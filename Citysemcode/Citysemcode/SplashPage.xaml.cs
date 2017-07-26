ca1124a2e9c0d32b6483c47cd6f41e59e8f39b96
magluz <magluz@live.com>
2017-07-26 16:17:05 -0300

    inicial

---
+++ Citysemcode/Citysemcode/SplashPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Citysemcode
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SplashPage : ContentPage
+	{
+
+        public SplashPage ()
+		{
+			InitializeComponent ();
+
+            Func<bool> convertMethod = NavigateSplash;
+
+            Device.StartTimer(TimeSpan.FromSeconds(5), convertMethod);
+        }
+
+        public bool NavigateSplash()
+        {
+            Navigation.PushAsync(new MainPage());
+            return true;
+        }
+	}
+}
